44ccf5872cd5e5ee4fe7b923f02e2fece12790e0
couto0 <daniel.couto64@gmail.com>
2023-01-09 17:10:07 -0300

    criado modelo de usuario

---
+++ models.py
@@ -0,0 +1,22 @@
+import uuid
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class User(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    username: str = Field(...)
+    name: str = Field(...)
+    email: str = Field(...)
+    birthday: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "username": "MFDOOM",
+                "email": "mfdoom@gmail.com",
+                "name": "Daniel Dumile",
+                "birthday": "1971-7-13"
+            }
+        }
+
