dde61c2375c2e96556eab99077915940651074f0
couto0 <daniel.couto64@gmail.com>
2023-01-10 00:23:39 -0300

    testes adicionais

--- test_main.py
+++ test_main.py
@@ -1,9 +1,11 @@
 from fastapi.testclient import TestClient
-
+import pytest
 from main import app
 
-
+@pytest.mark.skip(reason="will fail if user already exists.")
 def test_create_user():
+    """Create a new user"""
+
     with TestClient(app) as client:
         response = client.post(
             "/user/",
@@ -16,7 +18,9 @@ def test_create_user():
         )
         assert response.status_code == 201
         
-def test_create_invalid_username():
+def test_create_user_invalid_username():
+    """Create a new user with a invalid username"""
+
     with TestClient(app) as client:
         response = client.post(
             "/user/",
@@ -30,4 +34,40 @@ def test_create_invalid_username():
         assert response.status_code == 400
         assert response.json() == {
             "detail": "Invalid username."
+        }
+
+def test_create_user_invalid_email():
+    """Create a new user with a invalid email"""
+
+    with TestClient(app) as client:
+        response = client.post(
+            "/user/",
+            json={
+                    "username": "MFDOOM",
+                    "email": "mfdoomgmail.com",
+                    "name": "Daniel Dumile",
+                    "birthday": "1971-7-13"
+                },
+        )
+        assert response.status_code == 400
+        assert response.json() == {
+            "detail": "Invalid email address."
+        }
+
+def test_create_user_invalid_birthday():
+    """Create a new user with a invalid birthday"""
+
+    with TestClient(app) as client:
+        response = client.post(
+            "/user/",
+            json={
+                    "username": "MFDOOM",
+                    "email": "mfdoom@gmail.com",
+                    "name": "Daniel Dumile",
+                    "birthday": "1971-2-30"
+                },
+        )
+        assert response.status_code == 400
+        assert response.json() == {
+            "detail": "Invalid birthday, should be YYYY-MM-DD"
         }
